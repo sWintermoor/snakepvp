d39d6360b0b8881caea0615eb31f52bf3993f901
LaCrHa <lasse.harms@studium.uni-hamburg.de>
2024-09-13 11:26:27 +0200

    world

--- snake-pvp-world.rkt
+++ snake-pvp-world.rkt
@@ -3,71 +3,48 @@
 (require 2htdp/image)
 (require test-engine/racket-tests)
 
-; Verbesserungsvorschläge: Anstelle von first, second und so weiter lieber aussagekräftigere Funktionen definieren.
-
 ; WorldState is (Snakes Food Score)
-
-; Snakes is
-
-; Food is
-
-; Score is a Number
-
-
+; Verbesserungsvorschläge: Anstelle von first, second und so weiter lieber aussagekräftigere Funktionen definieren.
 
 ; Structs
 (define-struct item [type x y] #:prefab)
-(define-struct snake [coordinates color status direction velocity score inventory] #:prefab)
-(define-struct world [snakes items timer status] #:prefab) ;status: waiting, playing, win, loose
+(define-struct snake [coordinates color status direction velocity score banana] #:prefab)
+(define-struct world [snakes items timer status] #:prefab) ; status: waiting, playing, win, loose
 
 ; Startzustand der Welt
-(define WORLD0 (world '() '() 300 "waiting")) ;die Werte sind potenziell irrelevant
+(define WORLD0 (world '() '() 300 "waiting")) ; Startzustand, Werte sind potenziell irrelevant
 
 ; Spielfeldparameter
-(define GRID-SIZE 35)            ; 15x15 Felder
+(define GRID-SIZE 15)            ; 15x15 Felder
 (define CELL-SIZE 30)            ; Jede Zelle ist 30x30 Pixel groß
 (define WIDTH (* GRID-SIZE CELL-SIZE))  ; Gesamtbreite des Spielfelds
 (define HEIGHT (* GRID-SIZE CELL-SIZE)) ; Gesamthöhe des Spielfelds
-(define GRID-COLOR "black")
-
-(define FOOD-COLOR "red") ;Food-Color muss weg, anstelle dessen müssen wir type von item nutzen
-
-(define NUM_PLAYERS 2)
-
+(define EXTRA-HEIGHT 100)        ; Extra Platz für Timer, Banana-Counter und Scores
+(define TOTAL-HEIGHT (+ HEIGHT EXTRA-HEIGHT)) ; Gesamthöhe inklusive zusätzlichem Platz
 
 ; Empfangen von Nachrichten
-; Wir brauchen: Snakes (x, y, c) Food-Koordinaten, Score
-#|(check-expect ((receive '('('(3 3 "blue") '(3 4 "blue")) '('(7 5 "green") '(7 6 "green"))) '( 1 1) 9)
-               '('('('(3 4 "blue") '(3 5 "blue")) '('(7 6 "green") '(7 7 "green"))) '( 1 1) 9))
-              '('('('(3 4 "blue") '(3 5 "blue")) '('(7 6 "green") '(7 7 "green"))) '( 1 1) 9))|#
-
 (define (receive w m)
-  (let
-      ([snakes (first m)]
-       [items (second m)]
-       [timer (third m)]
-       [world_status (fourth m)])
+  (let ([snakes (first m)]
+        [items (second m)]
+        [timer (third m)]
+        [world_status (fourth m)])
     (world snakes items timer world_status)))
 
-
 ; Funktion zum Zeichnen des Gitternetzes
-
-
 (define (draw-grid scene)
   (foldl
    (lambda (i scene)
-     (let ([line-x (line 0 (* HEIGHT 2) GRID-COLOR)]   ; Vertikale Linien
-           [line-y (line (* WIDTH 2) 0 GRID-COLOR)])   ; Horizontale Linien
+     (let ([line-x (line 0 (* HEIGHT 2) "black")]  ; Vertikale Linien
+           [line-y (line (* WIDTH 2) 0 "black")]) ; Horizontale Linien
        (place-image line-x (* i CELL-SIZE) 0
                     (place-image line-y 0 (* i CELL-SIZE) scene))))
    scene
-   (range (+ 1 GRID-SIZE))))  ; Zeichnet das Gitter inklusive der Randlinien
+   (range (+ 1 GRID-SIZE))))  ; Zeichnet das Gitter inklusive Randlinien
 
 ; Zeichnet eine Zelle mit einer bestimmten Farbe
 (define (draw-cell x y color)
   (place-image (rectangle CELL-SIZE CELL-SIZE "solid" color)
-               (* x CELL-SIZE)
-               (* y CELL-SIZE)
+               (* x CELL-SIZE) (* y CELL-SIZE)
                (empty-scene WIDTH HEIGHT)))
 
 ; Zeichnet die Schlange
@@ -76,7 +53,7 @@
    (lambda (pos image)
      (let ([x (first pos)]
            [y (second pos)])
-       (place-image (rectangle CELL-SIZE CELL-SIZE status color) 
+       (place-image (rectangle CELL-SIZE CELL-SIZE status color)
                     (+ (/ CELL-SIZE 2) (* x CELL-SIZE))
                     (+ (/ CELL-SIZE 2) (* y CELL-SIZE))
                     image)))
@@ -85,64 +62,80 @@
 
 ; Zeichnet das Futter und weitere Items
 (define (draw-foods foods scene)
-  
   (foldl
    (lambda (fruit image)
      (let ([x (item-x fruit)]
-        [y (item-y fruit)]
-        [type (item-type fruit)])
-    (cond
-      [(eq? type 'apple)  (place-image (circle (/ CELL-SIZE 2) "solid" "red")
-                 (+ (/ CELL-SIZE 2) (* x CELL-SIZE))
-                 (+ (/ CELL-SIZE 2) (* y CELL-SIZE))
-                 image)]
-      [(eq? type 'banana)  (place-image (circle (/ CELL-SIZE 2) "solid" "yellow")
-                 (+ (/ CELL-SIZE 2) (* x CELL-SIZE))
-                 (+ (/ CELL-SIZE 2) (* y CELL-SIZE))
-                 image)]
-    )))
+           [y (item-y fruit)]
+           [type (item-type fruit)])
+       (cond
+         [(eq? type 'apple) (place-image (circle (/ CELL-SIZE 2) "solid" "red")
+                                         (+ (/ CELL-SIZE 2) (* x CELL-SIZE))
+                                         (+ (/ CELL-SIZE 2) (* y CELL-SIZE))
+                                         image)]
+         [(eq? type 'banana) (place-image (circle (/ CELL-SIZE 2) "solid" "yellow")
+                                          (+ (/ CELL-SIZE 2) (* x CELL-SIZE))
+                                          (+ (/ CELL-SIZE 2) (* y CELL-SIZE))
+                                          image)])))
    scene
    foods))
 
-; Zeichnet den Punktestand
-(define (draw-score score1 score2 time scene)
-  (place-image (text (format "Score-P1: ~a\nScore-P2: ~a\nTime: ~a" score1 score2 time) 20 "blue")
-               (+ CELL-SIZE 30) (/ CELL-SIZE 0.5)
+; Zeichnet den Timer (links oben unter dem Spielfeld)
+(define (draw-timer timer scene)
+  (place-image (text (format "Time: ~a" timer) 20 "black")
+               10 ; Links platziert
+               (- TOTAL-HEIGHT 90) ; Position unter dem Spielfeld
+               scene))
+
+; Zeichnet den Bananen-Zähler (rechts oben unter dem Spielfeld)
+(define (draw-banana bananacount scene)
+  (place-image (text (format "Banana: ~a" bananacount) 20 "black")
+               (- WIDTH 80) ; Rechts platziert
+               (- TOTAL-HEIGHT 90) ; Position unter dem Spielfeld
+               scene))
+
+; Zeichnet die Spieler-Scores (zentriert unter dem Timer und dem Bananen-Zähler)
+(define (draw-score score1 score2 scene)
+  (place-image (text (format "Score-P1: ~a  |  Score-P2: ~a" score1 score2) 20 "blue")
+               (/ WIDTH 2) ; Zentriert
+               (- TOTAL-HEIGHT 50) ; Position unter dem Timer und Bananen-Zähler
                scene))
 
 ; Zeichnet den gesamten Spielzustand
 (define (draw-world w)
   (cond 
     [(string=? (world-status w) "playing")
-     ;(= (length (world-snakes w)) NUM_PLAYERS)
      (let ([snake1 (snake-coordinates (first (world-snakes w)))]
            [snake2 (snake-coordinates (second (world-snakes w)))]
            [color1 (snake-color (first (world-snakes w)))] 
            [color2 (snake-color (second (world-snakes w)))]
            [status1 (snake-status (first (world-snakes w)))]
-           [status2 (snake-status (first (world-snakes w)))]
+           [status2 (snake-status (second (world-snakes w)))]
            [score1 (snake-score (first (world-snakes w)))]
            [score2 (snake-score (second (world-snakes w)))]
-           [time (world-timer w)]
-           [foods (world-items w)]) ;zeichnet alle Items in der Liste, aber als roter Kreis (so)
-       (draw-score score1 score2 time
-                   (draw-snake snake2 color2 status2 ;Schlange wird mit Farbe gezeichnet
-                               (draw-snake snake1 color1 status1
-                                           (draw-foods foods
-                                                       (draw-grid (empty-scene WIDTH HEIGHT)))))))]
-    [(string=? (world-status w) "waiting") (overlay (text "Waiting... \n\nUniverse started?" 20 "orange") (empty-scene WIDTH HEIGHT))] ;Eine bessere Methode als "string=?" finden?
-    [(string=? (world-status w) "loose") (overlay (text "You have lost" 20 "red") (empty-scene WIDTH HEIGHT))]
-    [(string=? (world-status w) "win") (overlay (text "You have won" 20 "green") (empty-scene WIDTH HEIGHT))]
-    [else (overlay (text "rejected" 20 "blue") (empty-scene WIDTH HEIGHT))]
-    ))
+           [timer (world-timer w)]
+           [foods (world-items w)]
+           [bananacount (snake-banana (first (world-snakes w)))])
+       (draw-score score1 score2
+                   (draw-timer timer
+                               (draw-banana bananacount
+                                            (draw-snake snake2 color2 status2
+                                                        (draw-snake snake1 color1 status1
+                                                                    (draw-foods foods
+                                                                                (draw-grid (empty-scene WIDTH TOTAL-HEIGHT)))))))))]
+    [(string=? (world-status w) "waiting")
+     (overlay (text "Waiting... \n\nUniverse started?" 20 "orange") (empty-scene WIDTH TOTAL-HEIGHT))]
+    [(string=? (world-status w) "loose")
+     (overlay (text "You have lost" 20 "red") (empty-scene WIDTH TOTAL-HEIGHT))]
+    [(string=? (world-status w) "win")
+     (overlay (text "You have won" 20 "green") (empty-scene WIDTH TOTAL-HEIGHT))]
+    [else
+     (overlay (text "rejected" 20 "blue") (empty-scene WIDTH TOTAL-HEIGHT))]))
 
-
-; Versenden der Tastatureingabe
+; Tastatureingabe
 (define (key-handler w key)
   (make-package w
-  (cond
-    [(string=? (world-status w) "playing") key]))) ; evtl ist Fallunterscheidung unnötig
-
+                (cond
+                  [(string=? (world-status w) "playing") key])))
 
 ; Starte das Spiel
 (define (create-world worldname)
@@ -153,6 +146,7 @@
     [name worldname]
     [register LOCALHOST]))
 
+; Mehrere Welten starten
 (launch-many-worlds 
  (create-world "a")
  (create-world "b"))
