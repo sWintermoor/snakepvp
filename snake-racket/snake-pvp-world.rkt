450ba6153df00b07396facbe1082cc0b1233d8f5
Mark Oliver Krüger <mark.oliver.krueger@gmail.com>
2024-11-22 21:35:40 +0100

    new file structure

--- snake-racket/snake-pvp-world.rkt
+++ snake-racket/snake-pvp-world.rkt
@@ -234,6 +234,6 @@
     [on-key key-handler]                      ; Verarbeitet Tastatureingaben
     [name worldname]                          ; Name der Welt
     ;   [state #t]                                ; Optionaler Zustand (deaktiviert)
-    ;[register "ip-adress"]                 ; Optionaler Register
+    ;[register "192.168.2.117"]                 ; Optionaler Register
     ;[port 9092]                               ; Optionaler Port
     [register LOCALHOST]))                    ; Lokalhost als Standard
