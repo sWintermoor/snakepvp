9bbc85341c0447ed91e104c463e23b01431043b4
Mark Oliver Krüger <mark.oliver.krueger@gmail.com>
2024-11-22 22:25:06 +0100

    angepasster Webserver

--- snake-server/WebServer.py
+++ snake-server/WebServer.py
@@ -1,8 +1,12 @@
+import os
 import socket 
 from socket import *
 import sys # In order to terminate the program
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
+
+#Verzeichnis für statische Dateien
+BASE_DIR = os.path.join(os.getcwd(), '..', 'snake-static')
 
 #Prepare a sever socket
 serverSocket.bind(('0.0.0.0', 6603))
@@ -12,28 +16,45 @@ while True:
     #Establish the connection
     print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
+
+
     try:
+        #Empfange die Nachricht (also den Dateinamen)
         message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = f.read()
-        
-        #Send one HTTP header line into socket
-        connectionSocket.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n".encode())
 
-        #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
+        # Handle unterschiedliche Dateitypen
+        if filename == '/':
+            filename = '/templates/index.html'  # Standardseite ist index.html
+        if filename.endswith('.css'):
+            content_type = 'text/css'
+        elif filename.endswith('.html'):
+            content_type = 'text/html'
+        elif filename.endswith('.js'):
+            content_type = 'application/javascript'
+        else:
+            content_type = 'text/html'
+    
+        filepath = os.path.join(os.getcwd(), '..', filename[1:])
+
+        try:
+            with open(filepath, 'r') as f:
+                outputdata = f.read()
         
-        connectionSocket.close()
+            #Sende Header und Dateiinhalte
+            connectionSocket.send(f"HTTP/1.1 200 OK\r\nContent-Type: {content_type}\r\n\r\n".encode())
+            connectionSocket.send(outputdata.encode())
         
-    except IOError:
-        #Send response message for file not found
-        connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
-        connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>".encode())
-        
-        #Close client socket
+        except IOError:
+            #Send response message for file not found
+            connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
+            connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>".encode())
+            
+            #Close client socket
+            connectionSocket.close()
+
+    except Exception as e:
+        print(f"Error occured: {e}")
         connectionSocket.close()
 
 serverSocket.close()
